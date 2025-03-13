46c9cc239eeb0554ffe86a14318a52403875723a
Rin Iwai <rin@eyrin.jp>
2025-03-13 18:24:01 +0900

    仮

--- f446xx/include/stm32rcos/hal.hpp
+++ f446xx/include/stm32rcos/hal.hpp
@@ -4,14 +4,14 @@
 
 namespace stm32rcos {
 
-#ifdef HAL_UART_MODULE_ENABLED
-void *get_uart_context(UART_HandleTypeDef *huart);
-void set_uart_context(UART_HandleTypeDef *huart, void *context);
-#endif
-
 #ifdef HAL_CAN_MODULE_ENABLED
 void *get_bxcan_context(CAN_HandleTypeDef *hcan);
 void set_bxcan_context(CAN_HandleTypeDef *hcan, void *context);
+#endif
+
+#ifdef HAL_UART_MODULE_ENABLED
+void *get_uart_context(UART_HandleTypeDef *huart);
+void set_uart_context(UART_HandleTypeDef *huart, void *context);
 #endif
 
 } // namespace stm32rcos
