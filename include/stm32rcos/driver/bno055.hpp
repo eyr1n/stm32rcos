9ebc0eb48a50462ead2e44760de2f6cf0f759ab8
Rin Iwai <rin@eyrin.jp>
2025-03-16 04:50:51 +0900

    wip

--- include/stm32rcos/driver/bno055.hpp
+++ include/stm32rcos/driver/bno055.hpp
@@ -23,11 +23,11 @@ public:
     core::TimeoutHelper timeout_helper;
     while (!timeout_helper.is_timeout(timeout)) {
       uint8_t data = 0x00;
-      if (!write_reg(0x3D, &data, 1)) {
+      if (!write_register(0x3D, &data, 1)) {
         continue;
       }
       data = 0x08;
-      if (!write_reg(0x3D, &data, 1)) {
+      if (!write_register(0x3D, &data, 1)) {
         continue;
       }
       return true;
@@ -37,17 +37,14 @@ public:
 
   std::optional<Eigen::Quaternionf> get_quaternion() {
     std::array<int16_t, 4> data;
-    if (!read_reg(0x20, reinterpret_cast<uint8_t *>(data.data()), 8)) {
+    if (!read_register(0x20, reinterpret_cast<uint8_t *>(data.data()), 8)) {
       return std::nullopt;
     }
     return Eigen::Quaternionf{data[0] / 16384.0f, data[1] / 16384.0f,
                               data[2] / 16384.0f, data[3] / 16384.0f};
   }
 
-private:
-  peripheral::UART &uart_;
-
-  bool write_reg(uint8_t addr, const uint8_t *data, uint8_t size) {
+  bool write_register(uint8_t addr, const uint8_t *data, uint8_t size) {
     std::array<uint8_t, 4> buf{0xAA, 0x00, addr, size};
     uart_.flush();
     if (!uart_.transmit(buf.data(), buf.size(), 5)) {
@@ -62,7 +59,7 @@ private:
     return buf[0] == 0xEE && buf[1] == 0x01;
   }
 
-  bool read_reg(uint8_t addr, uint8_t *data, uint8_t size) {
+  bool read_register(uint8_t addr, uint8_t *data, uint8_t size) {
     std::array<uint8_t, 4> buf{0xAA, 0x01, addr, size};
     uart_.flush();
     if (!uart_.transmit(buf.data(), 4, 5)) {
@@ -76,6 +73,9 @@ private:
     }
     return uart_.receive(data, size, 5);
   }
+
+private:
+  peripheral::UART &uart_;
 };
 
 } // namespace driver
