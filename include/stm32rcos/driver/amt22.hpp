f7ed6e80230840b8df4777036e01d14b3d024b82
Rin Iwai <rin@eyrin.jp>
2025-03-19 00:40:55 +0900

    fix amt22

--- include/stm32rcos/driver/amt22.hpp
+++ include/stm32rcos/driver/amt22.hpp
@@ -39,21 +39,21 @@ public:
 
   std::optional<uint16_t> read_position() {
     std::array<uint8_t, 2> command{0x00, 0x00};
-    std::array<uint8_t, 2> data;
-    if (!send_command(command.data(), data.data(), command.size())) {
+    auto res = send_command(command);
+    if (!res) {
       return std::nullopt;
     }
-    return (data[0] << 8 | data[1]) &
+    return (res->at(0) << 8 | res->at(1)) &
            ((1 << core::to_underlying(resolution_)) - 1);
   }
 
   std::optional<int16_t> read_turns() {
     std::array<uint8_t, 4> command{0x00, 0xA0, 0x00, 0x00};
-    std::array<uint8_t, 4> data;
-    if (!send_command(command.data(), data.data(), command.size())) {
+    auto res = send_command(command);
+    if (!res) {
       return std::nullopt;
     }
-    int16_t turns = (data[2] << 8 | data[3]) & 0x3FFF;
+    int16_t turns = (res->at(2) << 8 | res->at(3)) & 0x3FFF;
     if (turns & 0x2000) {
       turns |= 0xC000;
     }
@@ -62,14 +62,12 @@ public:
 
   bool set_zero_point() {
     std::array<uint8_t, 2> command{0x00, 0x70};
-    std::array<uint8_t, 2> data;
-    return send_command(command.data(), data.data(), command.size());
+    return send_command(command).has_value();
   }
 
   bool reset() {
     std::array<uint8_t, 2> command{0x00, 0x60};
-    std::array<uint8_t, 2> data;
-    return send_command(command.data(), data.data(), command.size());
+    return send_command(command).has_value();
   }
 
 private:
@@ -79,30 +77,33 @@ private:
   core::Semaphore tx_rx_sem_{1, 1};
   AMT22Resolution resolution_;
 
-  bool send_command(const uint8_t *command, uint8_t *data, size_t size) {
+  template <size_t N>
+  std::optional<std::array<uint8_t, N>>
+  send_command(const std::array<uint8_t, N> &command) {
+    std::array<uint8_t, N> rx_buf;
     HAL_GPIO_WritePin(cs_port_, cs_pin_, GPIO_PIN_RESET);
-    for (size_t i = 0; i < size; ++i) {
+    for (size_t i = 0; i < N; ++i) {
       tx_rx_sem_.try_acquire(0);
-      if (HAL_SPI_TransmitReceive_IT(hspi_, command + i, data + i,
+      if (HAL_SPI_TransmitReceive_IT(hspi_, &command[i], &rx_buf[i],
                                      sizeof(uint8_t))) {
         HAL_SPI_Abort_IT(hspi_);
-        return false;
+        return std::nullopt;
       }
       if (!tx_rx_sem_.try_acquire(1)) {
         HAL_SPI_Abort_IT(hspi_);
-        return false;
+        return std::nullopt;
       }
     }
     HAL_GPIO_WritePin(cs_port_, cs_pin_, GPIO_PIN_SET);
-    for (size_t i = 0; i < size; i += 2) {
-      if (!test_checksum(data[i], data[i + 1])) {
-        return false;
+    for (size_t i = 0; i < N; i += 2) {
+      if (!test_checksum(rx_buf[i], rx_buf[i + 1])) {
+        return std::nullopt;
       }
     }
-    return true;
+    return rx_buf;
   }
 
-  bool test_checksum(uint8_t l, uint8_t h) {
+  static inline bool test_checksum(uint8_t l, uint8_t h) {
     bool k1 = !(bit(h, 5) ^ bit(h, 3) ^ bit(h, 1) ^ bit(l, 7) ^ bit(l, 5) ^
                 bit(l, 3) ^ bit(l, 1));
     bool k0 = !(bit(h, 4) ^ bit(h, 2) ^ bit(h, 0) ^ bit(l, 6) ^ bit(l, 4) ^
@@ -110,7 +111,7 @@ private:
     return (k1 == bit(h, 7)) && (k0 == bit(h, 6));
   }
 
-  bool bit(uint8_t x, uint8_t i) { return ((x >> i) & 1) == 1; }
+  static inline bool bit(uint8_t x, uint8_t i) { return ((x >> i) & 1) == 1; }
 };
 
 } // namespace driver
