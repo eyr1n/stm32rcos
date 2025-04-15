6a3bafab88e8e84f00dcb61a88e87a834b167612
Rin Iwai <rin@eyrin.jp>
2025-04-16 05:25:35 +0900

    fix

--- include/stm32rcos/core/utility.hpp
+++ include/stm32rcos/core/utility.hpp
@@ -1,28 +1,12 @@
 #pragma once
 
-#include <cstddef>
 #include <cstdint>
-#include <cstdio>
-#include <string>
-#include <type_traits>
 
 #include <FreeRTOS.h>
 #include <task.h>
 
 namespace stm32rcos {
 namespace core {
-
-template <class T>
-constexpr std::underlying_type_t<T> to_underlying(T value) noexcept {
-  return static_cast<std::underlying_type_t<T>>(value);
-}
-
-template <class... Args> std::string format(const char *fmt, Args... args) {
-  size_t size = std::snprintf(nullptr, 0, fmt, args...);
-  std::string buf(size, '\0');
-  std::snprintf(buf.data(), size + 1, fmt, args...);
-  return buf;
-}
 
 class TimeoutHelper {
 public:
